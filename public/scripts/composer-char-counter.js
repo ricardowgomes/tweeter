c817bbe3ed51377c27b5028737ae4fff3ae3bd29
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-07-13 21:26:22 +0000

    added counter to textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+  // Variables of input
+  let $nodeNumMax = $("#counter");
+  const $numMax = $("#counter").text();
+  const $textarea = $("#tweet-text");
+  let $numInput = 0;
+
+  // Grab typing
+  $textarea.on('keypress propertychange change click keyup input paste', () => {
+    const $textareaValue = $textarea.val();
+    $numInput = $textareaValue.length;
+
+    // clears the counter
+    $nodeNumMax.empty();
+
+    const $count = $numMax - $numInput;
+    $nodeNumMax.append($count);
+
+    // Add red color to the counter when get to zero and disable the button
+    if ($count <= 0) {
+      $('output.counter').addClass('overcount');
+      $('.tweet-btn').attr('disabled', 'disabled');
+
+    } else {
+      $('output.counter').removeClass('overcount');
+      $('.tweet-btn').removeAttr('disabled');
+    }
+  });
+});
+
+
