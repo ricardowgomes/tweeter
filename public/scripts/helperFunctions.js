054df4786368765a69d47d1b4a116723e6799cf4
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-07-14 21:08:56 +0000

    tweets generated by JS

--- public/scripts/helperFunctions.js
+++ public/scripts/helperFunctions.js
@@ -1,4 +1,3 @@
-// const { renderPosts } = require('./helperFunctions');
 const createPost = (post) => {
   const $articleTweets = $('.display-tweets');
 
@@ -41,33 +40,4 @@ const renderPosts = (posts) => {
   }
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-$(document).ready(() => {
-  const $tweets = renderPosts(data);
-  $('.display-tweets').append($tweets);
-});
+module.exports = { renderPosts };
